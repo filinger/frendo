3e533dda8e7c96183e160f1c74ee43df1c00b5ea
General Failure <mathematics.max@gmail.com>
2016-05-17 19:29:45 +0300

    Added editorconfig

--- src/main/java/com/technoirarts/FrendoApplication.java
+++ src/main/java/com/technoirarts/FrendoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FrendoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrendoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FrendoApplication.class, args);
+    }
 }
