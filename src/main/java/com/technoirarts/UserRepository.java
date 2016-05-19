0996d7b8fdb1157706e0ce6f4e2e9c2ff20c50cd
TARGETrus <mousebreaker@mail.ru>
2016-05-19 11:45:23 +0300

    preparations for jpa specification usage.

--- src/main/java/com/technoirarts/UserRepository.java
+++ src/main/java/com/technoirarts/UserRepository.java
@@ -1,6 +1,7 @@
 package com.technoirarts;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>, JpaSpecificationExecutor {
 }
