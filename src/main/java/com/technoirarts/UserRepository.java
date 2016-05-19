0e65472eeca991e158651f9b0032851ac1f2dbda
TARGETrus <mousebreaker@mail.ru>
2016-05-19 13:24:03 +0300

    interface updated for jpa-specification User support.

--- src/main/java/com/technoirarts/UserRepository.java
+++ src/main/java/com/technoirarts/UserRepository.java
@@ -3,5 +3,5 @@ package com.technoirarts;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long>, JpaSpecificationExecutor {
+public interface UserRepository extends CrudRepository<User, Long>, JpaSpecificationExecutor<User> {
 }
