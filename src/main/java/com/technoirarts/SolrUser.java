c52352d503478c969d78807e8a694e11127638a3
TARGETrus <mousebreaker@mail.ru>
2016-05-22 23:55:05 +0300

    age updated.

--- src/main/java/com/technoirarts/SolrUser.java
+++ src/main/java/com/technoirarts/SolrUser.java
@@ -14,8 +14,17 @@ public class SolrUser {
     @Field("userId_l")
     private Long userId;
 
+    @Field("surname_s")
+    private String surname;
+
     @Field("name_s")
     private String name;
+
+    @Field("age")
+    private Integer age;
+
+    @Field("city_s")
+    private String city;
 
     @Field("extra_en")
     private String extra;
@@ -23,7 +32,10 @@ public class SolrUser {
     static public SolrUser fromUser(User user) {
         SolrUser solrUser = new SolrUser();
         solrUser.setUserId(user.getId());
+        solrUser.setSurname(user.getSurname());
         solrUser.setName(user.getName());
+        solrUser.setAge(user.getAge());
+        solrUser.setCity(user.getCity());
         solrUser.setExtra(user.getExtra());
         return solrUser;
     }
@@ -44,12 +56,36 @@ public class SolrUser {
         this.userId = userId;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 
     public String getExtra() {
